e859d9433f9be0848fc11d6235382361bba78cd4
Iván Mervich <imervich@gmail.com>
2014-07-28 10:58:03 -0500

    Initial Commit

---
+++ RainbowTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RainbowTable
+//
+//  Created by Iván Mervich on 7/28/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RainbowTable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RainbowTable
+//
+//  Created by Iván Mervich on 7/28/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
